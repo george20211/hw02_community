0dba6fc7e9ec54f4864c6347abc7ecf0ceac2a65
george20211 <79093366+george20211@users.noreply.github.com>
2021-04-17 17:59:28 +0300

    update

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -11,9 +11,9 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name="posts")
     group = models.ForeignKey('Group',
-                              on_delete=models.PROTECT,
+                              on_delete=models.SET_NULL,
                               blank=True, null=True,
-                              related_name='groups')
+                              related_name='posts')
 
     def __str__(self):
         return self.text
